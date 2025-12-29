a8787b54909929908fa8698bde683fc397bfc50b
SK AMAN <skaman.dev03@gmail.com>
2025-12-30 01:15:17 +0530

    Backend file structure uploaded

---
+++ finledger-backend/src/main/java/com/finledger/finledgerbackend/FinledgerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.finledger.finledgerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinledgerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinledgerBackendApplication.class, args);
+    }
+
+}
---
+++ finledger-backend/src/test/java/com/finledger/finledgerbackend/FinledgerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finledger.finledgerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinledgerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
